07c3252838bc5e95c42d7c3df016fabca630d6d5
Aberratio <joannakokot97@gmail.com>
2025-02-09 20:40:11 +0100

    configure iframe video

--- components/Exercise/VideoPlayer.tsx
+++ components/Exercise/VideoPlayer.tsx
@@ -1,12 +1,17 @@
 'use client';
 
-import Video from 'next-video';
 import { AspectRatio } from '@mantine/core';
 
 export const VideoPlayer = ({ url }: { url: string }) => {
   return (
-    <AspectRatio ratio={16 / 9} style={{ width: '100%', maxWidth: '700px', margin: '0 auto' }}>
-      <Video src={url} />
+    <AspectRatio ratio={16 / 9}>
+      <iframe
+        src={url}
+        title="My video player"
+        style={{ border: 0, maxWidth: '700px', margin: '0 auto' }}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen; webkitallowfullscreen; mozallowfullscreen"
+        allowFullScreen
+      />
     </AspectRatio>
   );
 };
